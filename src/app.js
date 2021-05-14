2af61b6e7a10fa81b5dc662be1db41c08654660e
okandavut <davut.okan@gmail.com>
2021-05-14 11:38:38 +0300

    Add icons to tab menu

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import * as React from 'react';
 import {Text, SafeAreaView, View} from 'react-native';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import CoffeeListScreen from './views/coffeelist';
@@ -13,9 +15,28 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Kahveler" component={CoffeeListScreen}  />
-        <Tab.Screen name="Kahve Tarihi" component={CoffeeHistoryScreen} />
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Kahveler') {
+            iconName = focused
+              ? 'coffee'
+              : 'coffee-outline';
+          } else if (route.name === 'Kahve Tarihi') {
+            iconName = 'history';
+          }
+
+          return <MaterialIcon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'black',
+        inactiveTintColor: 'gray',
+      }}>
+        <Tab.Screen name="Kahveler" component={CoffeeListScreen}    />
+        <Tab.Screen name="Kahve Tarihi" component={CoffeeHistoryScreen}   />
       </Tab.Navigator>
     </NavigationContainer>
   );
